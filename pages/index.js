5e24cb4a6be950159038859ab8ed24530de6109c
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-03 16:43:05 +0330

    connect to the api and  get course from api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Course from '@/components/templates/index/Course'
+import coureModel from '@/models/course';
+import connectToDB from '@/utils/db'
 import React from 'react'
 
 function index() {
   return (
     <Course />
   )
+}
+
+
+export async function getStaticProps(context) {
+  connectToDB();
+  
+  const courses = await coureModel.find({})
+  console.log(courses);
+
+  return {
+    props: {}
+  }
 }
 
 export default index
