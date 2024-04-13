c2903f1f47261574c7599f5edd61b41f5eaaacf3
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-13 13:37:46 +0330

    coding comments model

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const coursesModel = require("@/models/course");
+
+const schema = mongoose.Schema({
+
+    body: {
+        type: String,
+        required: true,
+    },
+    course: {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+        required: true
+    }
+},
+    {
+        timestamps: true
+    })
+
+const model = mongoose.models.Comment || mongoose.model("Comment", schema)
+export default model;
