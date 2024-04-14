fb3950f93712e90da28adf7271f9a60d2d0331a1
mahdiLeo1992 <mahdi.leo2014@gmail.com>
2024-04-14 16:14:36 +0330

    coding middleware

---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+    if (request.nextUrl.pathname.startsWith('/users')) {
+
+        // validation
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+
+    return NextResponse.next();
+}
